8acd7d91a3ff4e32eaaca82211dd714faacef0bd
glennTejada <90943706+glennTejada@users.noreply.github.com>
2022-03-10 15:21:26 +0600

    entry api implemented

---
+++ app/Http/Controllers/api/ApiController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function entry(Request $request){
+
+        User::create([
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'suburb' => $request->suburb,
+            'number' => $request->number,
+            'product' => $request->product,
+            'receipt' => $request->receipt,
+            'email' => $request->email,
+            'age' => $request->age,
+            'proof' => $request->proof,
+            'affiliates' => $request->affiliates,
+        ]);
+        $data = [
+            "apiKey" => "61fa033a-c163-4d57-9a84-9952ec525812",
+            "transactionId" => "123456",
+            "entrantId" => ""
+        ];
+
+        $client = new Client([
+            'headers' => ['Content-Type' => 'x-www-form-urlencoded']
+        ]);
+
+        $response= $client->request("POST","https://pistol-01-yvwa6gxw.instantwinapi.com/api/entry",['form_params' => $data]);
+        $result = json_decode($response->getBody());
+        return response()->json($result);
+    }
+}
