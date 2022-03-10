f6d821dc9255f55f3d8de232630372fe308cfa06
glennTejada <90943706+glennTejada@users.noreply.github.com>
2022-03-10 18:11:55 +0600

    change the api method

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/entry',[ApiController::class,'entry']);
+Route::post('/entry',[ApiController::class,'entry']);
 Route::get('/status',[ApiController::class,'status']);
